c9a4385759da96237d08135c4814e135166dc15e
youngG209 <46043956+youngG209@users.noreply.github.com>
2022-02-21 17:41:28 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/model/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.model;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/test/java/jpabook/jpashop/model/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("MemberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo( member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
